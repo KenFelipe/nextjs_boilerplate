14c2e645190caf1d7e5eaf00ff89ec79634385ae
Felipe Kenzo <Kenzo12152@gmail.com>
2021-05-30 20:49:46 -0300

    add @/utils module aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
-import { theme } from '../styles/theme'
-import { GlobalStyle } from '../styles/global'
+import { theme } from '@/styles/theme'
+import { GlobalStyle } from '@/styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
