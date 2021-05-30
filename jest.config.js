dfc4c706de8a7c9c92a1ecc9d2430e771b143988
Felipe Kenzo <Kenzo12152@gmail.com>
2021-05-30 00:30:12 -0300

    set module aliases

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,12 @@ module.exports = {
     '/public/',
     '/template/',
   ],
+
+  moduleNameMapper: {
+    '^@/atoms/(.*)$': '<rootDir>/src/components/atoms/$1',
+    '^@/molecules/(.*)$': '<rootDir>/src/components/molecules/$1',
+    '^@/organisms/(.*)$': '<rootDir>/src/components/organisms/$1',
+    '^@/templates/(.*)$': '<rootDir>/src/components/templates/$1',
+    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
 }
