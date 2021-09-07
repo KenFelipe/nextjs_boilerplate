e5be89d6cd8210f80947dfbe2812a26782d42dab
Felipe Kenzo <Kenzo12152@gmail.com>
2021-09-07 12:19:38 -0300

    add aliases to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,5 +6,18 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/atoms": path.resolve(__dirname, "../src/components/atoms"),
+      "@/molecules": path.resolve(__dirname, "../src/components/molecules"),
+      "@/organisms": path.resolve(__dirname, "../src/components/organisms"),
+      "@/templates": path.resolve(__dirname, "../src/components/templates"),
+      "@/api": path.resolve(__dirname, "../src/api"),
+      "@/styles": path.resolve(__dirname, "../src/styles"),
+      "@/utils": path.resolve(__dirname, "../src/utils"),
+    }
+    return config;
+  }
 }
