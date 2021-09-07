f35d78e9b87ed2934c792fe577ed2c0b440abbdf
Felipe Kenzo <Kenzo12152@gmail.com>
2021-09-07 12:48:22 -0300

    add src/api alias

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,7 @@ module.exports = {
     '^@/molecules/(.*)$': '<rootDir>/src/components/molecules/$1',
     '^@/organisms/(.*)$': '<rootDir>/src/components/organisms/$1',
     '^@/templates/(.*)$': '<rootDir>/src/components/templates/$1',
+    '^@/api/(.*)$': '<rootDir>/src/api/$1',
     '^@/styles/(.*)$': '<rootDir>/src/styles/$1',
     '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
   },
